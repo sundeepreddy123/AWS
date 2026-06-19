e1c10aae7d794fa29ea4a3c358f194614c4e449d
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2026-06-19 13:14:33 +0530

    feat: migrate Karpenter setup to manual configuration with dedicated files

--- eks.tf
+++ eks.tf
@@ -105,22 +105,3 @@ module "eks" {
   }
 }
 
-
-module "karpenter" {
-  source = "terraform-aws-modules/eks/aws//modules/karpenter"
-
-  cluster_name = module.eks.cluster_name
-
-  node_iam_role_use_name_prefix   = false
-  node_iam_role_name              = var.cluster_name
-  create_pod_identity_association = true
-  # Attach additional IAM policies to the Karpenter node IAM role
-  node_iam_role_additional_policies = {
-    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-  }
-
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
-  }
-}
