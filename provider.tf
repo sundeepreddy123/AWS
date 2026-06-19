d8afb4017f995717bc8ace9bc977d07ca4b785a6
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2026-06-19 21:53:51 +0530

    feat: add kubectl provider configuration for EKS token retrieval

--- provider.tf
+++ provider.tf
@@ -37,6 +37,27 @@ provider "kubernetes" {
   token = data.aws_eks_cluster_auth.this.token
 }
 
+provider "kubectl" {
+  host                   = aws_eks_cluster.this.endpoint
+
+  cluster_ca_certificate = base64decode(
+    aws_eks_cluster.this.certificate_authority[0].data
+  )
+
+  load_config_file = false
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      aws_eks_cluster.this.name
+    ]
+  }
+}
 # provider "helm" {
 #   kubernetes = {
 #     host                   = module.eks.cluster_endpoint
