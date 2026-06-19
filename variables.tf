9a70b4b1090c2b3401d42d177100ed9de2f5dd06
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2026-06-19 14:27:16 +0530

    feat: restore VPC module and outputs configuration

--- variables.tf
+++ variables.tf
@@ -12,19 +12,3 @@ variable "project_name" {
   description = "Project name"
   type        = string
 }
-
-variable "iam_role_arn" {
-  description = "IAM role ARN"
-  type        = string
-}
-
-variable "cluster_name" {
-  description = "EKS cluster name"
-  type        = string
-}
-
-variable "cluster_version" {
-  description = "EKS cluster version"
-  type        = string
-}
-
