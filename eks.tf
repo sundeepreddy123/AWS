24a223bc59098963aec79849d83491669ec148d1
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2026-06-19 12:59:38 +0530

    feat: add Karpenter module configuration to EKS setup

--- eks.tf
+++ eks.tf
@@ -104,3 +104,20 @@ module "eks" {
     "karpenter.sh/discovery" = var.cluster_name
   }
 }
+
+
+module "karpenter" {
+  source = "terraform-aws-modules/eks/aws//modules/karpenter"
+
+  cluster_name = module.eks.cluster_name
+
+  # Attach additional IAM policies to the Karpenter node IAM role
+  node_iam_role_additional_policies = {
+    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
