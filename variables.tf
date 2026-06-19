0a5c531c953e1cfec32e90893942c913ea8e3d85
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2026-06-19 10:24:25 +0530

    remove_null

--- variables.tf
+++ variables.tf
@@ -1,17 +1,14 @@
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = NULL
 }
 
 variable "environment" {
   description = "Deployment environment"
   type        = string
-  default     = NULL
 }
 
 variable "project_name" {
   description = "Project name"
   type        = string
-  default     = NULL
 }
