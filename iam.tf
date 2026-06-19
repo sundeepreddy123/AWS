39e240fbe5edd3e7bd97a982e8623ea4961b0a40
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2026-06-19 19:29:32 +0530

    feat: add IAM roles and policies for EKS cluster and Fargate integration

--- iam.tf
+++ iam.tf
@@ -1,3 +1,4 @@
+// eks_cluster-role
 resource "aws_iam_role" "eks_cluster" {
 
   name = "${var.cluster_name}-cluster-role"
@@ -18,10 +19,40 @@ resource "aws_iam_role" "eks_cluster" {
     ]
   })
 }
-
+// policy attachment for eks_cluster role
 resource "aws_iam_role_policy_attachment" "eks_cluster" {
 
   role = aws_iam_role.eks_cluster.name
 
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+
+// fargate-role
+
+resource "aws_iam_role" "fargate" {
+
+  name = "${var.cluster_name}-fargate-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [{
+      Effect = "Allow"
+
+      Principal = {
+        Service = "eks-fargate-pods.amazonaws.com"
+      }
+
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+// policy attachment for fargate role
+resource "aws_iam_role_policy_attachment" "fargate" {
+
+  role = aws_iam_role.fargate.name
+
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
 }
