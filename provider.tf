8c87d3850bc5f387a5eca7474bb94bdbea58ccc1
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2026-06-19 21:23:39 +0530

    refactor: update helm provider configuration in provider.tf

--- provider.tf
+++ provider.tf
@@ -15,14 +15,16 @@ data "aws_eks_cluster_auth" "this" {
   name = aws_eks_cluster.this.name
 }
 
-provider "kubernetes" {
-  host = data.aws_eks_cluster.this.endpoint
+provider "helm" {
+  kubernetes = {
+    host = data.aws_eks_cluster.this.endpoint
 
-  cluster_ca_certificate = base64decode(
-    data.aws_eks_cluster.this.certificate_authority[0].data
-  )
+    cluster_ca_certificate = base64decode(
+      data.aws_eks_cluster.this.certificate_authority[0].data
+    )
 
-  token = data.aws_eks_cluster_auth.this.token
+    token = data.aws_eks_cluster_auth.this.token
+  }
 }
 
 provider "kubernetes" {
