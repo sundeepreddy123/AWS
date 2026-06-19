f77c16892828a232814d4259cce8663e919a9f5b
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2026-06-19 10:20:26 +0530

    fix: update Terraform plan and apply steps to use variable files and reset default values in variables

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "ap-south-1"
+  default     = NULL
 }
 
 variable "environment" {
   description = "Deployment environment"
   type        = string
-  default     = "dev"
+  default     = NULL
 }
 
 variable "project_name" {
   description = "Project name"
   type        = string
-  default     = "rsr-infra"
+  default     = NULL
 }
