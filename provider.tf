19ea3a3f0db8406231db8543aec956c0de90aee2
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2026-06-20 22:29:08 +0530

    feat: add data sources for EKS cluster and authentication

--- provider.tf
+++ provider.tf
@@ -6,6 +6,13 @@ terraform{
   backend "s3" {}
 }
 
+data "aws_eks_cluster" "this" {
+  name = aws_eks_cluster.this.name
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = aws_eks_cluster.this.name
+}
 
 provider "helm" {
   kubernetes = {
