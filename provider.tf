97487fdc37a14ea3ee92f9695bb4ea0e8d42aa48
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2026-06-20 22:37:05 +0530

    fix: update EKS cluster name references in data sources

--- provider.tf
+++ provider.tf
@@ -7,11 +7,11 @@ terraform{
 }
 
 data "aws_eks_cluster" "this" {
-  name = aws_eks_cluster.this.name
+  name = aws_eks_cluster.dev.name
 }
 
 data "aws_eks_cluster_auth" "this" {
-  name = aws_eks_cluster.this.name
+  name = aws_eks_cluster.dev.name
 }
 
 provider "helm" {
