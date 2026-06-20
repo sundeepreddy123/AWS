d1c975827b5a3a19f700993a8c44ef1c8a335201
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2026-06-20 23:01:16 +0530

    fix: correct role-arn reference for AWS Load Balancer Controller service account

--- sa.tf
+++ sa.tf
@@ -9,7 +9,7 @@ resource "kubernetes_service_account" "alb_controller" {
 
     annotations = {
 
-      "eks.amazonaws.com/role-arn" = aws_iam_role.alb_controller.arn
+      "eks.amazonaws.com/role-arn" = aws_iam_role.aws_load_balancer_controller.arn
     }
   }
 }
