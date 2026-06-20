58abe5b9b36363b90d79c13ab265d969f4e68275
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2026-06-20 11:00:44 +0530

    added_dev

--- outputs.tf
+++ outputs.tf
@@ -15,17 +15,17 @@ output "az_zones" {
 }
 
 output "cluster_name" {
-  value = aws_eks_cluster.this.name
+  value = aws_eks_cluster.dev.name
 }
 
 output "cluster_endpoint" {
-  value = aws_eks_cluster.this.endpoint
+  value = aws_eks_cluster.dev.endpoint
 }
 
 output "cluster_security_group_id" {
-  value = aws_eks_cluster.this.vpc_config[0].cluster_security_group_id
+  value = aws_eks_cluster.dev.vpc_config[0].cluster_security_group_id
 }
 
 output "oidc_issuer" {
-  value = aws_eks_cluster.this.identity[0].oidc[0].issuer
+  value = aws_eks_cluster.dev.identity[0].oidc[0].issuer
 }
