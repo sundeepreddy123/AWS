240e492ad13d093a2a540427ed3dc967fdd98f4b
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2026-06-20 20:51:07 +0530

    feat: add nginx deployment and service configuration

--- data.tf
+++ data.tf
@@ -14,3 +14,20 @@ data "aws_subnets" "private" {
 
   }
 }
+
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.dev.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    data.tls_certificate.eks.certificates[0].sha1_fingerprint
+  ]
+
+  url = aws_eks_cluster.dev.identity[0].oidc[0].issuer
+}
