11a7878c4d99beea115569a1cefdfe942254909f
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2026-06-21 21:51:14 +0530

    fix: comment out TLS certificate and IAM OIDC provider configurations in data.tf

--- data.tf
+++ data.tf
@@ -15,19 +15,19 @@ data "aws_subnets" "private" {
   }
 }
 
-data "tls_certificate" "eks" {
-  url = aws_eks_cluster.dev.identity[0].oidc[0].issuer
-}
+# data "tls_certificate" "eks" {
+#   url = aws_eks_cluster.dev.identity[0].oidc[0].issuer
+# }
 
-resource "aws_iam_openid_connect_provider" "eks" {
+# resource "aws_iam_openid_connect_provider" "eks" {
 
-  client_id_list = [
-    "sts.amazonaws.com"
-  ]
+#   client_id_list = [
+#     "sts.amazonaws.com"
+#   ]
 
-  thumbprint_list = [
-    data.tls_certificate.eks.certificates[0].sha1_fingerprint
-  ]
+#   thumbprint_list = [
+#     data.tls_certificate.eks.certificates[0].sha1_fingerprint
+#   ]
 
-  url = aws_eks_cluster.dev.identity[0].oidc[0].issuer // This is the OIDC issuer URL for the EKS cluster, which is used to configure the IAM OIDC provider for service account authentication. that makes the IAM OIDC provider trust the exact HTTPS certificate for your EKS OIDC issuer.connection 
-  }
+#   url = aws_eks_cluster.dev.identity[0].oidc[0].issuer // This is the OIDC issuer URL for the EKS cluster, which is used to configure the IAM OIDC provider for service account authentication. that makes the IAM OIDC provider trust the exact HTTPS certificate for your EKS OIDC issuer.connection 
+#   }
