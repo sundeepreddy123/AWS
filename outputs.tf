ef96ec0b31bbbbfc3d05668a45cf0d651d005e6d
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2026-06-21 21:48:27 +0530

    fix: comment out EKS cluster output variables in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -14,18 +14,18 @@ output "az_zones" {
   value = var.az_zones
 }
 
-output "cluster_name" {
-  value = aws_eks_cluster.dev.name
-}
+# output "cluster_name" {
+#   value = aws_eks_cluster.dev.name
+# }
 
-output "cluster_endpoint" {
-  value = aws_eks_cluster.dev.endpoint
-}
+# output "cluster_endpoint" {
+#   value = aws_eks_cluster.dev.endpoint
+# }
 
-output "cluster_security_group_id" {
-  value = aws_eks_cluster.dev.vpc_config[0].cluster_security_group_id
-}
+# output "cluster_security_group_id" {
+#   value = aws_eks_cluster.dev.vpc_config[0].cluster_security_group_id
+# }
 
-output "oidc_issuer" {
-  value = aws_eks_cluster.dev.identity[0].oidc[0].issuer
-}
+# output "oidc_issuer" {
+#   value = aws_eks_cluster.dev.identity[0].oidc[0].issuer
+# }
