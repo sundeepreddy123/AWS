84cefb1c8b4f5a7da48893f16ab07403e63b7551
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2026-06-22 16:25:32 +0530

    fix: update subnet ID reference and format security group IDs in EC2 instance configuration

--- ec2.tf
+++ ec2.tf
@@ -5,11 +5,9 @@ resource "aws_instance" "ec2" {
 
   instance_type = var.ec2_instance_type
 
-  subnet_id = aws_subnet.public_1.id
+  subnet_id = aws_subnet.public[0].id
 
-  vpc_security_group_ids = [
-    aws_security_group.ec2.id
-  ]
+  vpc_security_group_ids = [aws_security_group.ec2.id]
 
   iam_instance_profile = aws_iam_instance_profile.ec2_ssm.name
 
