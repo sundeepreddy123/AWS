c6bcdc71fdcccb9cfb42b78cbc7dc7acd4dab2e9
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2026-06-27 21:39:04 +0530

    fix: update cluster_name reference in EKS node group configuration

--- eks.tf
+++ eks.tf
@@ -24,7 +24,7 @@ resource "aws_eks_cluster" "eks" {
 resource "aws_eks_node_group" "general" {
 
 
-  cluster_name = aws_eks_cluster.eks
+  cluster_name = aws_eks_cluster.eks.name
 
 
   node_group_name = "general"
