350b9fc3a998f26597052713b07ba96d360d310f
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2026-06-28 10:47:49 +0530

    feat: add initial configuration for ArgoCD with LoadBalancer service

---
+++ argocd.tf
@@ -0,0 +1,13 @@
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+
+  timeout = 900
+
+  values = [
+    file("argocd-values.yaml")
+  ]
+}
