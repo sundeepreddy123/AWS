24eaed3f2aa25673ccebe1c24a766ec6e9fcf82a
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2026-07-01 13:55:11 +0530

    refactor: uncomment and restore Helm and Kubernetes provider configurations

--- provider.tf
+++ provider.tf
@@ -7,27 +7,27 @@ terraform{
 }
 
 
-# provider "helm" {
-#   kubernetes = {
-#     host = data.aws_eks_cluster.eks.endpoint
+provider "helm" {
+  kubernetes = {
+    host = data.aws_eks_cluster.eks.endpoint
 
-#     cluster_ca_certificate = base64decode(
-#       data.aws_eks_cluster.eks.certificate_authority[0].data
-#     )
+    cluster_ca_certificate = base64decode(
+      data.aws_eks_cluster.eks.certificate_authority[0].data
+    )
 
-#     token = data.aws_eks_cluster_auth.eks.token
-#   }
-# }
+    token = data.aws_eks_cluster_auth.eks.token
+  }
+}
 
-# provider "kubernetes" {
-#   host = data.aws_eks_cluster.eks.endpoint
+provider "kubernetes" {
+  host = data.aws_eks_cluster.eks.endpoint
 
-#   cluster_ca_certificate = base64decode(
-#     data.aws_eks_cluster.eks.certificate_authority[0].data
-#   )
+  cluster_ca_certificate = base64decode(
+    data.aws_eks_cluster.eks.certificate_authority[0].data
+  )
 
-#   token = data.aws_eks_cluster_auth.eks.token
-# }
+  token = data.aws_eks_cluster_auth.eks.token
+}
 
 # provider "kubectl" {
 #   host                   = aws_eks_cluster.this.endpoint
