bb49464ad1e9c32a904eb629a84ba9b17033fc28
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2026-07-01 20:44:14 +0530

    fix: correct vpcId value syntax in helm_release configuration in alb.tf

--- alb.tf
+++ alb.tf
@@ -73,8 +73,9 @@ resource "helm_release" "aws_lbc" {
       value = "128Mi"
       }, {
       name  = "vpcId"
-      value = "aws_vpc.main.id"
-  }]
+      value = "${aws_vpc.main.id}"
+      }
+  ]
 
   depends_on = [aws_eks_node_group.general]
 }
