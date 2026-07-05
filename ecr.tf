b6f27405dc37957f6ef708b816b3cb7ab4afd64f
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2026-07-05 15:00:18 +0530

    refactor: update istio values file and fix annotation typo; add ECR repository configuration

---
+++ ecr.tf
@@ -0,0 +1,13 @@
+resource "aws_ecr_repository" "Application" {
+  name = "nginx"
+
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+}
