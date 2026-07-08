c6701712db0ae575177bfa750f4b4308b8bbb1cd
sundeepreddy123 <r.sundeepreddy786@gmail.com>
2026-07-08 10:58:44 +0530

    refactor: add ECR repository configuration in ecr.tf

---
+++ ecr.tf
@@ -0,0 +1,30 @@
+locals {
+  repositories = [
+    "adservice",
+    "cartservice",
+    "checkoutservice",
+    "emailservice",
+    "frontend",
+    "loadgenerator",
+    "paymentservice",
+    "productcatalogservice",
+    "recommendationservice",
+    "shippingservice"
+  ]
+}
+
+resource "aws_ecr_repository" "application" {
+  for_each = toset(local.repositories)
+
+  name = each.value
+
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+}
